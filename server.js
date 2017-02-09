2dd67daef6bc7e4d8d48b47cca56bf16a4210125
Vedank0909 <vedank0909@gmail.com>
2017-02-09 03:00:20 -0500

    changing port

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ let express = require('express');
 let app = express();
 
 // assigned a port to a constant
-const port = 3000;
+const localport = 3000;
 
 // setup the event loop - listens on port 3000
-app.listen(port);
-console.log(`Server listening at port: ${port}`);
+let port = process.env.PORT|| localport;
+app.set('port',port);
+app.listen('port',port);
+//console.log(`Server listening at port: ${port}`);
 
 // mounted two routes on the server
 
