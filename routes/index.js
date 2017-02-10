9d2cfc1ad9b871a15ad1bfd27663821fffd382ad
Vedank0909 <vedank0909@gmail.com>
2017-02-10 03:17:53 -0500

    added ejs pages

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,43 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+//let mongoose = require('mongoose');
+//let game = require('../config/game');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home'
+   });
 });
+
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'About' });
+  res.render('content/about', {
+    title: 'About'
+   });
 });
 
 
 
+
+
+/* GET products page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', {
+    title: 'Projects'
+   });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', {
+    title: 'Services'
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact'
+   });
+});
 
 module.exports = router;
